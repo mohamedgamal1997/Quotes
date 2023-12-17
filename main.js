adf3857e6e2303b0f113109aad3db93a6e2029c4
Ibrahim <ibrahimnasser07@gmail.com>
2023-12-17 18:43:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+ 
+
+var quotes =  [{
+  quote: `"You only live once, but if you do it right, once is enough."`,
+  writer: `– Mae West`
+}, {
+  quote: `"If you want to live a happy life, tie it to a goal, not to people or things."`,
+  writer: `– Albert Einstein`
+}, {
+  quote: `"Never let the fear of striking out keep you from playing the game."`,
+  writer: `– Babe Ruth`
+}, {
+  quote: `"Your time is limited, so don’t waste it living someone else’s life."`,
+  writer: `– Steve Jobs`
+}, {
+  quote: `"In order to write about life first you must live it."`,
+  writer: `– Ernest Hemingway`
+}, {
+  quote: `"Life is not a problem to be solved, but a reality to be experienced."`,
+  writer: `– Soren Kierkegaard`
+}, {
+  quote: `"The unexamined life is not worth living."`,
+  writer: `– Socrates`
+}, {
+  quote: `"Turn your wounds into wisdom."`,
+  writer: `– Oprah Winfrey`
+}, {
+  quote: `"The purpose of our lives is to be happy."`,
+  writer: `- Dalai Lama`
+}, {
+  quote: `"Live for each second without hesitation."`,
+  writer: `- Elton John`
+}, ]
+
+
+var quote = document.getElementById(".quote");
+
+var writer = document.getElementById(".writer");
+
+function quotesDisplay(){
+    var randomNumber = Math.floor(Math.random()*(quotes.length));
+    document.getElementById('quote').innerHTML= quotes[randomNumber].quote;
+    document.getElementById('writer').innerHTML= quotes[randomNumber].writer;
+}
